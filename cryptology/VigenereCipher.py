d6b714dc3da4a4d2c3801ee6d5474958f1b4345c
odiouschipmunk <odiouschipmunk@gmail.com>
2024-09-17 21:31:40 -0400

    vignere cipher

--- cryptology/VigenereCipher.py
+++ cryptology/VigenereCipher.py
@@ -1,41 +1,61 @@
-#VigenereCipher.py
-#import CaesarCipher
-
+# VigenereCipher.py
+from CaeserCipher import encrypt, decrypt
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def findKey(keyword, letterCount):
-    keyword.upper()
+    keyword = keyword.upper()
     spot = letterCount % len(keyword)
     key = alpha.find(keyword[spot])
-
     return key
 
-#use the encode/decode methods from your Caesar Cipher.
-#The key will change for each letter so you'll be encoding
-#a one-letter phrase using the Caesar Cipher and adding the results together.
-
-
-
-
-def main():
-
-    message = input("Enter message: ")
-    keyword = input("Enter keyword: ")
-
+def encode_vigenere(message, keyword):
     secret = ""
     letterCount = 0
 
     message = message.upper()
     for letter in message:
-
         key = findKey(keyword, letterCount)
 
-        if (alpha.find(letter) >= 0): #check to see if the letter is actually a letter
-            spot = (alpha.find(letter) + key) % 26
-            letterCount = letterCount + 1
-            secret = secret + alpha[spot]
-        else: # letter must have been a number, symbol, or punctuation.
-            secret = secret + letter
+        if alpha.find(letter) >= 0:  # check if the letter is alphabetic
+            encrypted_letter = encrypt(letter, key)  # Using Caesar Cipher's encrypt method
+            secret += encrypted_letter
+            letterCount += 1
+        else:
+            secret += letter  # Non-alphabetic characters remain unchanged
+
+    return secret
 
 
-    print ("Encoded message:", secret)
+def decode_vigenere(message, keyword):
+    secret = ""
+    letterCount = 0
+
+    message = message.upper()
+    for letter in message:
+        key = findKey(keyword, letterCount)
+
+        if alpha.find(letter) >= 0:
+            decrypted_letter = decrypt(letter, key)  # Using Caesar Cipher's decrypt method
+            secret += decrypted_letter
+            letterCount += 1
+        else:
+            secret += letter
+
+    return secret
+
+def main():
+    mode = input("Do you want to (e)ncode or (d)ecode? ").lower()
+    message = input("Enter message: ")
+    keyword = input("Enter keyword: ")
+
+    if mode == 'e':
+        encoded_message = encode_vigenere(message, keyword)
+        print("Encoded message:", encoded_message)
+    elif mode == 'd':
+        decoded_message = decode_vigenere(message, keyword)
+        print("Decoded message:", decoded_message)
+    else:
+        print("Invalid mode. Choose 'e' for encode or 'd' for decode.")
+
+if __name__ == "__main__":
+    main()
