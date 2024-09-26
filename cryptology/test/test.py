c6d980af7481481900a69fcb2338f200f8fc0730
odiouschipmunk <odiouschipmunk@gmail.com>
2024-09-26 09:09:12 -0400

    ready for crypto test!

--- cryptology/test/test.py
+++ cryptology/test/test.py
@@ -7,111 +7,14 @@ import random
 import string
 from tqdm import tqdm
 import itertools
-
-def brute_force_caesar(input_text):
-    highest_score=0
-    decrypt=""
-    k=0
-    results = []
-    for key in tqdm(range(1, 26)):  # Caesar cipher keys range from 1 to 25
-        decrypted_text = caesar.decrypt(input_text, key)
-        score=detect_english.is_english(decrypted_text, detect_english.dictionary, return_score=True)
-        if score>highest_score:
-            highest_score=score
-            decrypt=decrypted_text
-            k=key
-            results.append((highest_score, k, decrypt))
-    print(f'highest score for caesar:{highest_score}\ndecrypted text:{decrypt}\nkey:{k}')
-    return results
-
-
-
-def test_brute_force_substitution(input_text):
-    results = []
-    highest_score = 0
-    decrypt = ""
-    k = 0
-    alphabet = string.ascii_lowercase
-    
-    # Limit to the first 1000 permutations
-    top_keys = list(itertools.permutations(alphabet))[:1000]
-    
-    for key in tqdm([''.join(p) for p in top_keys]):
-        decrypted_text = substitution.decrypt(input_text, key)
-        score = detect_english.is_english(decrypted_text, detect_english.dictionary, return_score=True)
-        if score > highest_score:
-            highest_score = score
-            decrypt = decrypted_text
-            k = key
-            results.append((highest_score, decrypt, k))
-    
-    print(f'highest score for substitution: {highest_score}\ndecrypted text: {decrypt}\nkey: {k}')
-    return results
-
-
-def brute_force_substitution(input_text):
-    results = []
-    highest_score=0
-    decrypt=""
-    k=0
-    alphabet = string.ascii_lowercase
-    
-    for key in tqdm([''.join(p) for p in itertools.permutations(alphabet)]):
-        decrypted_text = substitution.decrypt(input_text, key)
-        score=detect_english.is_english(decrypted_text, detect_english.dictionary, return_score=True)
-        if score>highest_score:
-            highest_score=score
-            decrypt=decrypted_text
-            k=key
-            results.append((highest_score, decrypt, k))
-    print(f'highest score for substitution:{highest_score}\ndecrypted text:{decrypt}\nkey:{k}')
-    return results
-
-def test_brute_force_vigenere(input_text):
-    results = []
-    # Assuming keys are words of length 1 to 5 for simplicity
-    alphabet = string.ascii_lowercase
-    highest_score=0
-    decrypt=""
-    k=0
-    for key in tqdm([''.join(p) for p in itertools.product(alphabet, repeat=3)]):
-        decrypted_text = vigenere.decrypt(input_text, key)
-        score=detect_english.is_english(decrypted_text, detect_english.dictionary, return_score=True)
-        if score>highest_score:
-            highest_score=score
-            decrypt=decrypted_text
-            k=key
-            results.append((highest_score, decrypt, k))
-    print(f'highest score for vigenere:{highest_score}\ndecrypted text:{decrypt}\nkey:{k}')
-    return results
-
-
-def brute_force_vigenere(input_text):
-    results = []
-    # Assuming keys are words of length 1 to 5 for simplicity
-    alphabet = string.ascii_lowercase
-    highest_score=0
-    decrypt=""
-    k=0
-    for length in range(1,10):
-        for key in tqdm([''.join(p) for p in itertools.product(alphabet, repeat=length)]):
-            decrypted_text = vigenere.decrypt(input_text, key)
-            score=detect_english.is_english(decrypted_text, detect_english.dictionary, return_score=True)
-            if score>highest_score:
-                highest_score=score
-                decrypt=decrypted_text
-                k=key
-                results.append((highest_score, decrypt, k))
-    print(f'highest score for vigenere:{highest_score}\ndecrypted text:{decrypt}\nkey:{k}')
-    return results
-
+import brute_force
 def main():
     # Read input from a text file
     with open('input.txt', 'r') as file:
         input_text = file.read().strip()
 
     # Choose the action
-    action = input("Do you want to encrypt, decrypt, or brute force the message? (encrypt/decrypt/brute force): ").lower()
+    action = input("Do you want to encrypt, decrypt, or brute force the message? (encrypt/decrypt/brute force/brute force test): ").lower()
 
     if action == 'encrypt' or action == 'decrypt':
         # Choose the cipher method
@@ -164,12 +67,29 @@ def main():
         print("Letter Frequency:", letter_freq)
         print("Is the text English?", is_english)
 
+    elif action == 'brute force test':
+        # Choose the cipher method
+        total_results=[[]]
+        total_results.append(brute_force.brute_force_caesar(input_text))
+        total_results.append(brute_force.test_brute_force_vigenere(input_text))
+        total_results.append(brute_force.test_brute_force_substitution(input_text))
+
+        with open('brute_force_results.txt', 'w') as file:
+            for result in total_results:
+                for highest_score, decrypt, k in result:
+                    file.write(f"Highest score: {highest_score}\n")
+                    file.write(f"Key: {k}\n")
+                    file.write(f"Decrypted text: {decrypt}\n\n")
+        print("Brute force results written to brute_force_results.txt")
+    
+    
+    
     elif action == 'brute force':
         # Choose the cipher method
         total_results=[[]]
-        total_results.append(brute_force_caesar(input_text))
-        total_results.append(brute_force_vigenere(input_text))
-        total_results.append(brute_force_substitution(input_text))
+        total_results.append(brute_force.brute_force_caesar(input_text))
+        total_results.append(brute_force.brute_force_vigenere(input_text))
+        total_results.append(brute_force.brute_force_substitution(input_text))
 
         with open('brute_force_results.txt', 'w') as file:
             for result in total_results:
